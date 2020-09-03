dc830c540e3fd839913bc1eb2792a6a641b10b86
Rob <robert.a.nickel@gmail.com>
2020-09-04 11:56:53 +1200

    CharCtrlStarting

---
+++ Assets/01_Scripts/Player/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] string Vert;
+    [SerializeField] string Horiz;
+    public float speed;
+
+    Rigidbody rb;
+    GameObject Player;
+    CharacterController Char;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = this.gameObject;
+        rb = Player.GetComponent<Rigidbody>();
+        Char = Player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Move());
+    }
+
+
+
+
+
+    IEnumerator Move()
+    {
+        yield return new WaitForFixedUpdate();
+
+        float VertInput = Input.GetAxis(Vert);
+        float HorizInput = Input.GetAxis(Horiz);
+        Vector3 moveValues = new Vector3(HorizInput, 0, VertInput) * speed;
+
+        Char.SimpleMove(moveValues);
+
+    }
+
+}
