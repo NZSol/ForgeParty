b79fba38e4bcff7b30a611caeecb7a3e2c4b41b6
Rob <robert.a.nickel@gmail.com>
2020-09-08 11:29:33 +1200

    Character Controller
    Character Movement functioning correctly
    Character facing direction and maintaining direction
    Xbox 1, PS4 controllers working

--- Assets/01_Scripts/Player/Movement.cs
+++ Assets/01_Scripts/Player/Movement.cs
@@ -10,61 +10,54 @@ public class Movement : MonoBehaviour
 
     Rigidbody rb;
     GameObject Player;
-    CharacterController Char;
 
     public float maxSpeed = 5;
     public float minSpeed = -5;
 
-    Vector3 dir = new Vector3(0,0,0);
-    float rotSpeed = 3;
+
     public float damping;
-    Quaternion rotation;
 
     // Start is called before the first frame update
     void Start()
     {
         Player = this.gameObject;
         rb = Player.GetComponent<Rigidbody>();
-        Char = Player.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Move());
+        Move();
     }
 
-
-
-    public bool control;
+    Vector3 storedVel;
     Vector3 moveValues;
-
-    IEnumerator Move()
+    private void FixedUpdate()
     {
-        float VertInput = Input.GetAxis(Vert);
-        float HorizInput = Input.GetAxis(Horiz);
-        //Vector3 moveValues = new Vector3(HorizInput, 0, VertInput);
-        //float moveMag = Vector3.Magnitude(moveValues);
+        storedVel = moveValues * Time.deltaTime;
+        rb.velocity += (moveValues.normalized + storedVel) * (Time.deltaTime * speed);
+    }
 
-        if(control == true)
+    
+    void Move()
+    {
+        float VertInput = Input.GetAxisRaw(Vert);
+        float HorizInput = Input.GetAxisRaw(Horiz);
+        moveValues = new Vector3(HorizInput, 0, VertInput);
+
+
+
+
+        print(rb.velocity);
+
+        if (moveValues.magnitude != 0)
         {
-            moveValues = new Vector3(0, Mathf.Atan2(HorizInput, VertInput) * 180 / Mathf.PI, 0);
-        }
-        else
-        {
-            moveValues = new Vector3(HorizInput, 0, VertInput);
+            transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis(Horiz), Input.GetAxis(Vert)) * 180 / Mathf.PI, 0);
         }
 
 
 
-        if (rb.velocity.magnitude != 0)
-        {
-            //transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis(Horiz), Input.GetAxis(Vert)) * 180 / Mathf.PI, 0);
-        }
-
-        rb.velocity += moveValues;
-
-        ////Clamp RigidBody Velocity
+                    //Clamp RigidBody Velocity
         if (rb.velocity.magnitude > maxSpeed)
         {
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
@@ -74,37 +67,5 @@ public class Movement : MonoBehaviour
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, minSpeed);
         }
 
-
-        yield return null;
     }
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.GetComponent<Collider>().material != null)
-        {
-            rb.velocity = -rb.velocity;
-            print(collision.gameObject);
-            print(collision.gameObject.GetComponent<Collider>().material);
-
-        }
-    }
-
 }
-
-/*
-
-    IEnumerator Move()
-    {
-        yield return new WaitForFixedUpdate();
-
-        float VertInput = Input.GetAxis(Vert);
-        float HorizInput = Input.GetAxis(Horiz);
-        Vector3 moveValues = new Vector3(HorizInput, 0, VertInput) * speed;
-
-        Char.SimpleMove(moveValues);
-
-    }
-
-}
-*/
