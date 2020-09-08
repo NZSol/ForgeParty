8540b8df4b08877f0a6f8706685aa5fed97c5430
Drina <Drinadomandiola@gmail.com>
2020-09-09 11:49:56 +1200

    setting up player Confirm screens

---
+++ Assets/UI/Script/PanelReset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelReset : MonoBehaviour
+{
+
+    public GameObject panelToReset;
+
+    private void OnEnable()
+    {
+        panelToReset.GetComponent<Button>();
+        Debug.Log ("i got a button");
+    }
+}
--- Assets/playerConfirm.cs
+++ Assets/playerConfirm.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class playerConfirm : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,10 +14,5 @@ public class SceneManager : MonoBehaviour
     void Update()
     {
         
-    }
-        
-    public void HeadOut()
-    {
-        Application.Quit();
     }
 }
