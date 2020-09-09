b2de9cb01b64ee654ee0ba86ccb8230f82dbdf9d
Drina <Drinadomandiola@gmail.com>
2020-09-10 11:40:29 +1200

    Teams+Confirmation first steps

---
+++ Assets/UI/Script/PlayersList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersList : MonoBehaviour
+{
+    public PlayerButton[] Team1;
+    public PlayerButton[] Team2;
+   
+
+
+
+
+
+}
