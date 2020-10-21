16643063156ed80bded27ff2f2cc8937271d1090
Sopaipipalta <Drinadomandiola@gmail.com>
2020-10-22 11:50:08 +1300

    ScriptableSettingsBeginning

---
+++ Assets/UI/Script/scriptableObject/SetVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public Slider Volume;
+    public ScriptableObject SettingsValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    
+}
---
+++ Assets/UI/Script/scriptableObject/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings Values", menuName = "Settings")]
+public class Settings : ScriptableObject
+{
+    public float Volume = 100f;
+
+
+ }
