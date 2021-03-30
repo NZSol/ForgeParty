53128c7ee5c2bb728d023dda64a025fcfa0971d2
Rob <robert.a.nickel@gmail.com>
2021-03-31 11:32:50 +1300

    Code Input
    Added Input field to settings screen
    Unlock Tutor characters if correct code entered

---
+++ Assets/01_Scripts/Accessory/CheckCode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckCode : MonoBehaviour
+{
+    [SerializeField] InputField textInputField = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (LevelSelect.instance.setPrefFalse)
+        {
+            PlayerPrefs.SetInt("TutorsCode", 0);
+        }
+    }
+
+    public void enableTutors(string text)
+    {
+        if (text == "Tutors")
+        {
+            PlayerPrefs.SetInt("TutorsCode", 1);
+
+            textInputField.text = "Code Accepted";
+            textInputField.textComponent.horizontalOverflow = HorizontalWrapMode.Wrap;
+            print(PlayerPrefs.GetInt("TutorsCode"));
+        }
+        else
+        {
+            textInputField.text = string.Empty;
+        }
+    }
+}
